343d7a43fc92a37fb5bc4a02160eec2c8c121634
Tatsiana Iuchyk <tatsiana_iuchyk@epam.com>
2020-01-27 12:09:00 +0100

    lesson 3 - calculation of square and hypotinuse of the triange via 2 methods

---
+++ TanyaAuto/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TanyaAuto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lesson3_Pifagor();
+            Lesson3_Cos();
+            Console.ReadKey();
+        }
+
+        static void Lesson3_Pifagor()
+        {
+            double a = 5, b = 7;
+            double square;
+
+            square = a * b / 2;
+            Console.WriteLine("Square: " + square);
+
+            double hypotinuse = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            Console.WriteLine("Hypotinuse: " + hypotinuse);
+        }
+
+        static void Lesson3_Cos()
+        {
+            double a = 5, b = 7;
+            double hypotinuse;
+            double cos = Math.Cos(90 * Math.PI / 180);
+            hypotinuse = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2) - 2 * a * b * cos);
+            Console.WriteLine("Hypotinuse:" + hypotinuse);
+
+        }
+    }
+}
