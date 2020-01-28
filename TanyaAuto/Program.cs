66c8cd1cacb920104321ea9aed6c3348241ad6d8
Tatsiana Iuchyk <tatsiana_iuchyk@epam.com>
2020-01-28 10:20:54 +0100

    - added and deleted some lines where necesary; - added validation for negative - renamed some variables for readability

--- TanyaAuto/Program.cs
+++ TanyaAuto/Program.cs
@@ -34,23 +34,35 @@ namespace TanyaAuto
             hypotinuse = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2) - 2 * a * b * cos);
             Console.WriteLine("Hypotinuse:" + hypotinuse);
         }
+
         static void Lesson4_1()
         {
-            int a, b;
+            int localGoals, guestGoals;
             Console.WriteLine("Locals:");
-            a = Convert.ToInt32(Console.ReadLine());
+            localGoals = Convert.ToInt32(Console.ReadLine());
+            if (localGoals < 0)
+            {
+                Console.WriteLine("Goals cannot be negative");
+                return;
+            }
+            
             Console.WriteLine("Guests:");
-            b = Convert.ToInt32(Console.ReadLine());
-            if (a > b)
+            guestGoals = Convert.ToInt32(Console.ReadLine());
+            if (guestGoals < 0)
+            {
+                Console.WriteLine("Goals cannot be negative");
+                return;
+            }
+
+            if (localGoals > guestGoals)
                 Console.WriteLine("Locals won!!!");
-            else if (a < b)
+            else if (localGoals < guestGoals)
                 Console.WriteLine("Guests won!!!");
             else
                 Console.WriteLine("Dead Head!!!");
-
             Console.ReadKey();
-
         }
+
         static void Lesson4_2_1()
         {
             int a;
@@ -59,6 +71,7 @@ namespace TanyaAuto
             Console.WriteLine(a % 3 == 0 && a % 7 == 0 ? "Multiple of 7 and 3" : "Not multiple of 7 and 3");
             Console.ReadKey();
         }
+
         static void Lesson4_2_2()
         {
             int a;
@@ -70,6 +83,5 @@ namespace TanyaAuto
                 Console.WriteLine("Not multiple of 7 and 3");
             Console.ReadKey();
         }
-        
     }
 }
